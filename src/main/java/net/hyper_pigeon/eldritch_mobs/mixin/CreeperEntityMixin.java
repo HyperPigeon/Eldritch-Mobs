73e60df30ccafaf90f1ec0b996cab5efecf401e9
墨涤千尘 <leey319@163.com>
2021-12-12 00:14:32 +0800

    #issue_id：44

--- src/main/java/net/hyper_pigeon/eldritch_mobs/mixin/CreeperEntityMixin.java
+++ src/main/java/net/hyper_pigeon/eldritch_mobs/mixin/CreeperEntityMixin.java
@@ -3,8 +3,8 @@ package net.hyper_pigeon.eldritch_mobs.mixin;
 import dev.onyxstudios.cca.api.v3.component.ComponentProvider;
 import net.hyper_pigeon.eldritch_mobs.EldritchMobsMod;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.World;
@@ -13,8 +13,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Mixin(CreeperEntity.class)
-public class CreeperEntityMixin extends HostileEntity   {
+public class CreeperEntityMixin extends HostileEntity {
 
 
     protected CreeperEntityMixin(EntityType<? extends HostileEntity> entityType, World world) {
@@ -23,11 +26,14 @@ public class CreeperEntityMixin extends HostileEntity   {
 
 
     @Inject(at = @At("HEAD"), method = "explode")
-    public void removeEffects(CallbackInfo ci){
+    public void removeEffects(CallbackInfo ci) {
         if (!this.world.isClient && EldritchMobsMod.isElite((ComponentProvider) this)) {
-            for(StatusEffectInstance statusEffectInstance : this.getStatusEffects()){
+            // Cannot call remove in for-each
+            /*for(StatusEffectInstance statusEffectInstance : this.getStatusEffects()){
                 this.removeStatusEffect(statusEffectInstance.getEffectType());
-            }
+            }*/
+            List<StatusEffect> instances = this.getStatusEffects().stream().map(StatusEffectInstance::getEffectType).collect(Collectors.toList());
+            instances.forEach(this::removeStatusEffect);
         }
 
     }
