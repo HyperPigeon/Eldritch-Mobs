2e5e4b1b02085a1fd1b377a24a687bda74beca5f
hanth0m <129209706+hanth0m@users.noreply.github.com>
2026-04-17 22:28:02 +0200

    updated to 1.21.11

--- src/main/java/net/hyper_pigeon/eldritch_mobs/config/EldritchMobsModMenuIntegration.java
+++ src/main/java/net/hyper_pigeon/eldritch_mobs/config/EldritchMobsModMenuIntegration.java
@@ -3,11 +3,12 @@ package net.hyper_pigeon.eldritch_mobs.config;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class EldritchMobsModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return screen -> AutoConfig.getConfigScreen(EldritchMobsConfig.class, screen).get();
+        return screen -> AutoConfigClient.getConfigScreen(EldritchMobsConfig.class, screen).get();
     }
 }
